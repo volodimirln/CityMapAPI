22b83171d3e9f76b32b49515a716ff0feb05ad26
Volodimir <114944707+volodimirln@users.noreply.github.com>
2024-07-31 20:19:35 +0300

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,6 @@ namespace Tests;
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
-{
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
     public function createApplication()
     {
         return require __DIR__.'/../bootstrap/app.php';
